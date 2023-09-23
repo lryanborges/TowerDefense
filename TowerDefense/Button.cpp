8b36fca1f8bd797ed23f5af965c23ffe17acbc74
Paulo Holanda <paulorfholanda@gmail.com>
2023-09-23 18:58:08 -0300

    criação da classe button

---
+++ TowerDefense/Button.cpp
@@ -0,0 +1,94 @@
+#include "Button.h"
+#include "TowerDefense.h"
+
+Button::Button(int b){
+	t = b;
+
+	switch (t){
+
+	case REDB:
+		iconB = new Image("Resources/button/icon1B.png");
+		iconD = new Image("Resources/button/icon1D.png");
+		iconA = new Image("Resources/button/icon1A.png");
+		button = new Sprite(iconD);
+		break;
+	case PURPLEB:
+		iconA = new Image("Resources/button/icon2A.png");
+		iconD = new Image("Resources/button/icon2D.png");
+		iconB = new Image("Resources/button/icon2B.png");
+		button = new Sprite(iconD);
+		break;
+	case BLUEB:
+		iconA = new Image("Resources/button/icon3A.png");
+		iconD = new Image("Resources/button/icon3D.png");
+		iconB = new Image("Resources/button/icon3B.png");
+		button = new Sprite(iconD);
+		break;
+	case YELLOWB:
+		iconB = new Image("Resources/button/icon4B.png");
+		iconA = new Image("Resources/button/icon4A.png");
+		iconD = new Image("Resources/button/icon4D.png");
+		button = new Sprite(iconD);
+		break;
+	case GREENB:
+		iconA = new Image("Resources/button/icon5A.png");
+		iconD = new Image("Resources/button/icon5D.png");
+		iconB = new Image("Resources/button/icon5B.png");
+		button = new Sprite(iconD);
+		break;
+	default:
+		iconB = new Image("Resources/button/icon1B.png");
+		iconD = new Image("Resources/button/icon1D.png");
+		iconA = new Image("Resources/button/icon1A.png");
+		button = new Sprite(iconD);
+		t = REDB;
+		break;
+	}
+	
+//BBox(new Rect(-30, -30, 30, 30));
+
+type = BUTTON;
+}
+
+Button::~Button()
+{
+	delete iconA;
+
+	delete iconD;
+
+	delete iconB;
+
+	delete button;
+}
+
+void Button::Update(){
+	
+}
+
+void Button::Draw(){
+	
+	switch (t) {
+	case REDB:
+		button->Draw(610, 645, Layer::FRONT);
+		break;
+	case PURPLEB:
+		button->Draw(680, 645, Layer::FRONT);
+		break;
+	case BLUEB:
+		button->Draw(750, 645, Layer::FRONT);
+		break;
+	case YELLOWB:
+		button->Draw(820, 645, Layer::FRONT);
+		break;
+	case GREENB:
+		button->Draw(890, 645, Layer::FRONT);
+		break;
+	}
+
+
+}
+
+void Button::OnCollision(Object* obj){
+
+
+}
---
+++ TowerDefense/Button.h
@@ -0,0 +1,31 @@
+#include "Object.h"
+#include "Sprite.h"
+using std::stringstream;
+enum buttonTypes {
+	GREENB,
+	REDB,
+	YELLOWB,
+	BLUEB,
+	PURPLEB,
+};
+enum buttonState {
+	DESACTIVATE,
+	ACTIVATE
+};
+
+class Button : public Object {
+private:
+	Image* iconA;
+	uint t;
+	Image* iconD;
+	Image* iconB;
+	Sprite* button;
+public:
+	Button(int b);
+	~Button();
+	void Update();
+	void Draw();
+
+	void OnCollision(Object* obj);
+
+};
